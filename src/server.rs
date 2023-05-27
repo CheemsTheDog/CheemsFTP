6afd5e73abd01f02bd47d417041d3df7957ba294
cheemsburger <hornycheems2137@gmail.com>
2023-05-27 12:00:58 +0200

    continuation

--- src/server.rs
+++ src/server.rs
@@ -14,10 +14,17 @@ use std::thread::JoinHandle;
 #[allow(unused_imports)]
 use std::fs::{self, read, File};
 use std::vec;
-use std::process::Command;
+use std::process::{Command, Output};
+use std::fs::{OpenOptions, };
+use std::io::{Seek, SeekFrom,};
+
+use std::io;
+
+use std::os::windows::prelude::FileExt;
 // use self::logged_user::{LoggedUser, Privileges};
 
-use std::fs::{OpenOptions, remove_file};
+use std::fs::{remove_file};
+const BUF_LENGTH: usize = 1400;
 
 // mod logged_user;
 pub struct FtpServer {
@@ -58,6 +65,21 @@ pub struct FtpServer {
 //     command_type: Action
 // }
 
+pub struct RecvdCommand  {
+    command: String,
+    argument: String,
+    output: Option<String>,
+}
+
+impl RecvdCommand {
+    pub fn new(command:  String, argument: String, output: Option<String>) -> Self {
+        return  Self{
+            command,
+            argument, 
+            output
+        }
+    }
+} 
 impl FtpServer {
     pub fn new<A: ToSocketAddrs>(addr: A) -> Self {
 
@@ -103,6 +125,7 @@ impl FtpServer {
     // fn set_credentials(&mut self, path: String) { self.user_entries = path; }
 
     // fn set_root_dir(&mut self, path: String) { self.root_dir = path; }
+
     ///Starts server's listening
     pub fn start_listening(&mut self) {
         for connection in self.handler.incoming() {
@@ -118,75 +141,85 @@ impl FtpServer {
     }
 /// Performs user defined actions to the connected TCPStream
     pub fn handle_client(&self) {
+        use crate::server::capture_command;
         loop {
-            let mut output = self.receive_command();
-            Self::send_cmd_result(self.user.as_ref().unwrap(), output);
-            // Self::receive(
-            // self.user.as_ref().unwrap(),
-            // Self::touch(path)
-            // );
-            println!("Dupa");
+            self.handle_command( capture_command(self.user.as_ref().unwrap() ) )
         }
     }
-    /// General funtion to receive data from stream and save it in file
-    pub fn receive(mut stream: &TcpStream, mut file: File) {
-        let mut buffer  = [0; 1400];
-        loop {
-            let bytes_read = match stream.read(&mut buffer)
-            {
-                Ok(0) => return,
-                Ok(n) => file.write(&buffer[..n]),
-                Err(e) => {
-                    eprintln!("Failed to read from socket: {}", e);
-                    return;
+    pub fn handle_command(&self, command: RecvdCommand) {
+        use crate::server::send_file;
+        let mut path = self.cwd.borrow().clone();
+        match command.command.as_str() {
+            "cd" => {
+                match command.argument.as_str() {
+                    ".." => {
+                        let mut pos: usize = 0;
+                        for (i, c) in command.argument.chars().enumerate() {  
+                            if c == '/' {
+                                pos = i;
+                            }
+                        }
+                        (*self.cwd.borrow_mut()).truncate(pos);
+                    }
+                    "/." => {
+                        *self.cwd.borrow_mut() = self.root_dir.clone();
+                    }
+                    _  =>{
+                        if command.argument == 0 {
+                        
+                        }
+                    }
                 }
-            };
-        }
-    }
-    fn receive_command(&self) -> String{
-        let mut buffer: [u8; 1400]  = [0; 1400];
-        let mut parsed: String = String::new();
-        match self.user.as_ref().unwrap().read(&mut buffer) {
-            Ok(n) => {  
-                parsed = match std::str::from_utf8(&buffer[..n]) {
-                    Ok(string) => string.to_string(),
-                    Err(_) => panic!("Invalid UTF-8 sequence"),
-                };
+                
             }
-            Err(e) => {
-                eprintln!("Failed to read from socket: {}", e);
-                return "Error".to_string();
+            "dir" => {
+                
             }
-        };
-        parsed.pop();
-        parsed.pop();
-        if &parsed[..2] == "cd" {
-            let mut temp = String::new();
-            temp.push_str(&parsed);
-            temp.push_str(" && cd");
-            let output= Command::new("cmd")
-            .args(&["/C", &temp])
-            .output()
-            .expect("Failed to execute command");
-            *self.cwd.borrow_mut() = String::from_utf8_lossy(&output.stdout).to_string();
-            return self.cwd.borrow().clone();
+            "mkdir" => {
+    
+            }
+            "rmdir" => {
+    
+            }
+            "echo." => {
+    
+            }
+            "del" => {
+    
+            }
+            "download" => {
+                path.push_str("/");
+                path.push_str(&command.argument);
+                send_file(self.user.as_ref().unwrap(), path);
+            }
+            "upload" => {
+                let mut file_desc = OpenOptions::new()
+                .write(true)
+                .create_new(true)
+                .open(path)
+                .unwrap();
+                receive_file(self.user.as_ref().unwrap(), file_desc);
+            }
+            _ => {
 
+                //     OpenOptions::new()
+//     .write(true)
+//     .create_new(true)
+//     .open(&filepath)
+//     .unwrap();
+// // .open(DIR.to_owned()+"//test.txt")
+// // .open(filepath)
+// // .unwrap();
+    
+            }
         }
-        let mut command = String::from("cd ");
-        command.push_str(self.cwd.borrow().as_str());
-        command.push_str(" && ");
-        command.push_str(&parsed);
-        let output = Command::new("cmd")
-        .args(&["/C", &command])
-        .output()
-        .expect("Failed to execute command");
-        return String::from_utf8_lossy(&output.stdout).to_string();
     }
 
     fn send_cmd_result(mut stream: &TcpStream, mut result: String) {
         if result.is_empty() { result.push_str("Done"); }
         stream.write(&result.as_bytes()).unwrap();
-    } 
+    }
+} 
     // Creates a "name" dir with a cmd command at user's cwd
 //     pub fn mkdir(&self, dirname: String) {
 //         let mut command = String::from("cd ");
@@ -299,4 +332,105 @@ impl FtpServer {
 //         return Err(());
 
 // }
+///General purpose fn to send a file at path to stream.
+///Opens the file in standard read-only mode.
+///Absolute path must be provided
+pub fn send_file(mut stream: &TcpStream, path: String){
+    let mut file_toSend = OpenOptions::new()
+        .read(true)
+        .open(path)
+        .unwrap();
+    let mut buffer:[u8; BUF_LENGTH] = [0;BUF_LENGTH];
+    let mut start: u64 = 0;
+    let mut end: u64 = 0; 
+    let filesize = file_toSend.metadata().unwrap().len();
+    let mut filesize_left = file_toSend.metadata().unwrap().len();
+    println!("Filesize: {}", filesize);
+
+    loop {
+        if filesize_left < BUF_LENGTH as u64 {
+            if filesize_left == 0 { return; } 
+            // modulo = (BUF_LENGTH-(BUF_LENGTH-filesize_left as usize));
+            file_toSend.seek_read(&mut buffer[.. filesize_left as usize ], filesize-filesize_left).unwrap_or_default();
+            stream.write(&buffer[.. filesize_left as usize ]).unwrap_or_default();
+            println!("Bytes left: {}", filesize_left);
+            println!("Data transfered in 100%");
+            stream.shutdown(std::net::Shutdown::Both).unwrap_or_default();
+            return; }
+
+        else {
+            file_toSend.seek_read(&mut buffer[..], filesize-filesize_left).unwrap_or_default();
+            filesize_left-=BUF_LENGTH as u64;
+            stream.write(&buffer).unwrap_or_default(); 
+            println!("Bytes left: {}", filesize_left);
+            println!("Data transfered in {:.2}%", ( (filesize - filesize_left)*100/filesize ) as f64 ); }
+    }
+}
+/// General purpose funtion to receive data from stream and save it in a file.
+/// File descriptor so write options get be set.
+/// Undefined behaviour unless write/write_new is set to true;
+pub fn receive_file(mut stream: &TcpStream, mut file: File) {
+    let mut buffer  = [0; 1400];
+    loop {
+        let bytes_read = match stream.read(&mut buffer)
+        {
+            Ok(0) => return,
+            Ok(n) => file.write(&buffer[..n]),
+            Err(e) => {
+                eprintln!("Failed to read from socket: {}", e);
+                return;
+            }
+        };
+    }
+}
+
+/// Captures a packet with client's cmd command string.
+pub fn capture_command(mut stream: &TcpStream) -> RecvdCommand{
+    let mut buffer: [u8; 1400]  = [0; 1400];
+    let mut parsed: String = String::new();
+    match stream.read(&mut buffer) {
+        Ok(n) => {  
+            parsed = match std::str::from_utf8(&buffer[..n]) {
+                Ok(string) => string.to_string(),
+                Err(_) => return RecvdCommand::new("0".to_string(), "0".to_string(), None),
+            };
+        }
+        Err(e) => {
+            eprintln!("Failed to read from socket: {}", e);
+            return RecvdCommand::new("0".to_string(), "0".to_string(), None);
+        }
+    };
+    //cutting EOF sign
+    parsed.pop();
+    parsed.pop();
+    // Assuming received command is correct
+    for (i, c) in parsed.chars().enumerate() {
+        if c == ' ' {
+            return RecvdCommand::new((&parsed[..i]).to_string(), (&parsed[i+1..]).to_string(), None  );
+        }
+    }
+    // Else return command/_ with 0 argument. Possible breaches of security.
+    return RecvdCommand::new(parsed, "0".to_string(), None); 
+
+    // if &parsed[..2] == "cd" {
+    //     let mut temp = String::new();
+    //     temp.push_str(&parsed);
+    //     temp.push_str(" && cd");
+    //     let output= Command::new("cmd")
+    //     .args(&["/C", &temp])
+    //     .output()
+    //     .expect("Failed to execute command");
+    //     *self.cwd.borrow_mut() = String::from_utf8_lossy(&output.stdout).to_string();
+    //     return self.cwd.borrow().clone();
+
+    // }
+    // let mut command = String::from("cd ");
+    // command.push_str(self.cwd.borrow().as_str());
+    // command.push_str(" && ");
+    // command.push_str(&parsed);
+    // let output = Command::new("cmd")
+    // .args(&["/C", &command])
+    // .output()
+    // .expect("Failed to execute command");
+    // return String::from_utf8_lossy(&output.stdout).to_string();
 }
